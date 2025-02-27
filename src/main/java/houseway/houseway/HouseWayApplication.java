f5167ad7be5199a2cb68a93900c193dc510ed0a3
41817 <bangjungbin@gmail.com>
2025-02-27 16:47:49 +0900

    Initial commit

---
+++ src/main/java/houseway/houseway/HouseWayApplication.java
@@ -0,0 +1,13 @@
+package houseway.houseway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseWayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/houseway/houseway/HouseWayApplicationTests.java
@@ -0,0 +1,13 @@
+package houseway.houseway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

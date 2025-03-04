cd3ed88613f528e423f12313bd58cf1e297912eb
41817 <bangjungbin@gmail.com>
2025-03-04 15:30:51 +0900

    MODIFY/(미정)

--- src/main/java/houseway/houseway/controller/AdminController.java
+++ src/main/java/houseway/houseway/controller/AdminController.java
@@ -9,8 +9,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 
     @GetMapping("/")
+    public String login() {
+        return "views/admin/login";
+    }
+
+    @GetMapping("/index")
     public String index() {
         return "views/admin/index";
+    }
+
+
+    @GetMapping("/product")
+    public String product() {
+        return "views/admin/product";
+    }
+
+
+    @GetMapping("/member")
+    public String member() {
+        return "views/admin/member";
     }
 
 }

81ec3d259b8c368d0092b3c29f5cd704a15a916d
jungeun <jungeun0054@gmail.com>
2025-03-07 12:49:47 +0900

    수정

--- src/main/java/houseway/houseway/controller/UserController.java
+++ src/main/java/houseway/houseway/controller/UserController.java
@@ -12,14 +12,28 @@ import javax.servlet.http.HttpSession;
 
 @Slf4j  // lombok - logging객체 자동 생성
 @Controller
-@RequestMapping("/member") /* 이렇게 안쓴다면 아래에서 적용하면 됨 */
+@RequestMapping("/user") /* 이렇게 안쓴다면 아래에서 적용하면 됨 */
 @RequiredArgsConstructor
 public class UserController {
 
+@GetMapping("/search")
+    public String find() {
 
+    return "views/user/search";
+}
+@GetMapping("/estateDetail")
+    public String estateDetail() {
 
+    return "views/user/estateDetail";
+}
+@GetMapping("/reservation")
+    public String reservOk() {
 
+    return "views/user/reservation";
+}
 
-
-
+@GetMapping("/index_demo")
+    public String indexDemo() {
+    return "views/user/index_demo";
+}
 }

c12c67298a56b564e560910fea925bc4e1f7adcc
41817 <bangjungbin@gmail.com>
2025-03-11 16:17:08 +0900

    MODIFY/ Estate 리스트 페이지 사진 상단 뱃지 추가

--- src/main/java/houseway/houseway/domain/EstateListDTO.java
+++ src/main/java/houseway/houseway/domain/EstateListDTO.java
@@ -11,5 +11,6 @@ public class EstateListDTO {
     private String estate_addr;
     private int estate_deposit;
     private int estate_rent;
+    private int estate_viewCount;
     private int estate_state;
 }

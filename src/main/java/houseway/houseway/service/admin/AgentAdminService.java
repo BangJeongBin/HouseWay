2596eb3c61acde39fa511cd29521909907f34dc9
41817 <bangjungbin@gmail.com>
2025-03-12 11:42:17 +0900

    ADD/ agent.html 검색 기능 개발

--- src/main/java/houseway/houseway/service/admin/AgentAdminService.java
+++ src/main/java/houseway/houseway/service/admin/AgentAdminService.java
@@ -4,6 +4,8 @@ import houseway.houseway.domain.AgentInfoDTO;
 import houseway.houseway.domain.AgentPageDTO;
 import houseway.houseway.domain.UserPageDTO;
 
+import java.util.Map;
+
 public interface AgentAdminService {
 
     // 공인중개사 리스트(페이지네이션)
@@ -17,4 +19,10 @@ public interface AgentAdminService {
 
     // 공인중개사 삭제
     int removeAgent(int agentNum);
+
+    // 멤버 검색(페이지네이션)
+    AgentPageDTO findAgent(int cpg, String findtype, String findkey);
+
+    // 멤버 검색을 위한 카운트 메서드
+    int countFindAgent(Map<String, Object> params);
 }

42cfd557b6078ed62fc672c9d4695240316ca859
41817 <bangjungbin@gmail.com>
2025-03-12 11:20:20 +0900

    ADD/ member.html 검색 기능 개발

--- src/main/java/houseway/houseway/service/admin/MemberAdminService.java
+++ src/main/java/houseway/houseway/service/admin/MemberAdminService.java
@@ -3,6 +3,8 @@ package houseway.houseway.service.admin;
 import houseway.houseway.domain.UserInfoDTO;
 import houseway.houseway.domain.UserPageDTO;
 
+import java.util.Map;
+
 public interface MemberAdminService {
 
     // 멤버 리스트(페이지네이션)
@@ -16,4 +18,10 @@ public interface MemberAdminService {
 
     // 멤버 삭제
     int removeMember(String userId);
+
+    // 멤버 검색(페이지네이션)
+    UserPageDTO findMember(int cpg, String findtype, String findkey);
+
+    // 멤버 검색을 위한 카운트 메서드
+    int countFindMember(Map<String, Object> params);
 }

b4fd100aa502e7e1ec2a675f7620ea14f2f64bf7
min <fheka77@gmail.com>
2025-03-13 10:42:02 +0900

    충돌 수정

--- src/main/java/houseway/houseway/repository/user/ReservRepository.java
+++ src/main/java/houseway/houseway/repository/user/ReservRepository.java
@@ -2,13 +2,21 @@ package houseway.houseway.repository.user;
 
 import houseway.houseway.domain.MyReservDTO;
 import houseway.houseway.domain.Reserv;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper
 public interface ReservRepository {
+
+    @Insert("insert into reserv (user_id, estate_id,agent_num,agent_name,reserv_regdate) values (#{user_id},#{estate_id}, #{agent_num},#{agent_name},#{reserv_regdate})")
+    int saveReservation(String user_id, String estate_id, int agent_num, String agent_name, LocalDateTime reserv_regdate);
+
+    @Select("select * from reserv where user_id = #{user_id} order by reserv_regdate desc limit 1")
+    Reserv findByUserId(String user_id);
 
     @Select("select r.estate_id, a.agent_name, a.office_name, a.agent_phone, r.reserv_regdate, r.reserv_state" +
             " from reserv r join agent a on r.agent_num = a.agent_num" +
